070ad75602cbb22abe4713e3ede5c25b6021afcc
jueunk617 <juuuune62@naver.com>
2023-07-26 17:23:08 +0900

    Move repository to a new location

---
+++ app/src/main/java/com/example/guru2/Auth/JoinDialog.kt
@@ -0,0 +1,39 @@
+package com.example.guru2.Auth
+
+import androidx.appcompat.app.AppCompatActivity
+
+// 회원가입 페이지가 만들어지면 다시 수정..
+class JoinDialog : AppCompatActivity() {
+    //private lateinit var Auth: FirebaseAuth
+    //private lateinit var etEmail: EditText
+    //private lateinit var etPassword: EditText
+    //private lateinit var btnSignUp: Button
+
+    //override fun onCreate(savedInstanceState: Bundle?) {
+        //super.onCreate(savedInstanceState)
+        //setContentView(R.layout.joinDialog)
+
+        //Auth = Firebase.auth
+        //etEmail = findViewById(R.id.etEmail)
+        //etPassword = findViewById(R.id.etPassword)
+        //btnSignUp = findViewById(R.id.btnSignUp)
+
+        //btnSignUp.setOnClickListener {
+            //val email = etEmail.text.toString().trim()
+            //val password = etPassword.text.toString().trim()
+
+            //Auth.createUserWithEmailAndPassword(email, password)
+                //.addOnCompleteListener(this) { task ->
+                    //if (task.isSuccessful) {
+                        // 회원가입 성공
+                        //val user: FirebaseUser? = Auth.currentUser
+                        //Toast.makeText(this, "회원가입이 완료되었습니다. 다시 로그인해주세요.", Toast.LENGTH_SHORT).show()
+                        // 회원가입 후 홈화면으로 이동 추가
+                    //} else {
+                        // 회원가입 실패
+                        //Toast.makeText(this, "회원가입에 실패했습니다. 다시 시도해주세요.", Toast.LENGTH_SHORT).show()
+                    //}
+                //}
+        //}
+    //}
+}
