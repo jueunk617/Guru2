5d283b0148598be7944bf0af5eebd1fd4f3f7c59
jueunk617 <juuuune62@naver.com>
2023-07-29 01:32:56 +0900

    나도 참여하기 + 이전 버튼 + 플러스러스버튼 (주석)

--- app/src/main/java/com/example/guru2/Board/DeliveryActivity.kt
+++ app/src/main/java/com/example/guru2/Board/DeliveryActivity.kt
@@ -1,14 +1,34 @@
 package com.example.guru2.Board
 
+import android.app.Dialog
+import android.content.Intent
 import android.os.Bundle
+//import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.guru2.R
 
 class DeliveryActivity: AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.boardpage_delivery)
 
+        val boardpageIv1 = findViewById<ImageView>(R.id.boardpage_iv1)
+        //val frameDeliveryBtn = findViewById<Button>(R.id.frame_delivery)
+
+        boardpageIv1.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        //frameDeliveryButton.setOnClickListener {
+        //showAddPostDialog()
+        //}
+    }
+
+    private fun showAddPostDialog() {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.addpost_dialog)
+        dialog.show()
     }
 }
--- app/src/main/java/com/example/guru2/Board/PurchaseActivity.kt
+++ app/src/main/java/com/example/guru2/Board/PurchaseActivity.kt
@@ -1,14 +1,34 @@
 package com.example.guru2.Board
 
+import android.app.Dialog
+import android.content.Intent
 import android.os.Bundle
+//import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.guru2.R
 
 class PurchaseActivity: AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.boardpage_purchase)
 
+        val boardpage_iv1 = findViewById<ImageView>(R.id.boardpage_iv1)
+        //val framePurchaseBtn = findViewById<Button>(R.id.frame_purchase)
+
+        boardpage_iv1.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        //frameDeliveryButton.setOnClickListener {
+        //showAddPostDialog()
+        //}
+    }
+
+    private fun showAddPostDialog() {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.addpost_dialog)
+        dialog.show()
     }
 }
