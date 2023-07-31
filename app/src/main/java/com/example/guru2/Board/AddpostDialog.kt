24067445c45c9b5bef0727074a3f766e290a8836
jueunk617 <juuuune62@naver.com>
2023-07-31 19:48:26 +0900

    combined file

--- app/src/main/java/com/example/guru2/Board/AddpostDialog.kt
+++ app/src/main/java/com/example/guru2/Board/AddpostDialog.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.guru2.R
 import com.example.guru2.data.PostType
@@ -15,12 +16,10 @@ class AddpostDialog : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.addpost_dialog)
-        Log.i("##INFO", "testmessage : ");
+        Log.i("##INFO", "testmessage : ")
 
         dBtn = findViewById(R.id.addpost_dialog_btn1)
         pBtn = findViewById(R.id.addpost_dialog_btn2)
-
-
 
         dBtn.setOnClickListener {
             Log.i("##INFO", "dBtn.setOnClickListener")
@@ -28,6 +27,11 @@ class AddpostDialog : AppCompatActivity() {
         }
         pBtn.setOnClickListener {
             movePage(PostType.Purchase)
+        }
+
+        val xButton: ImageView = findViewById(R.id.imageView)
+        xButton.setOnClickListener {
+            finish()
         }
     }
 
